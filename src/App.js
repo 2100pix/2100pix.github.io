b7719f718c49f50457232b01a4953b844032eda7
Ramshal <ramshal15@hotmail.com>
2024-07-15 22:52:20 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="haiiii">
+      <a href="https://seintive.site">website: seintive.site</a>
+    </div>
+  );
+}
+
+export default App;
